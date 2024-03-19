83a6f1743af5d20840862f0840b21c3a5171fe64
Enzo G. Borrelli A <enzoborrellidev@gmail.com>
2024-03-19 19:43:06 -0300

    header options

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function Home() {
   return (
     <main className="size-full flex flex-col gap-2 justify-center items-center">
-      <h2 className="text-4xl text-text-100 font-bold uppercase">pagina de inicio</h2>
+      <div className="bg-text-200 size-fit flex gap-4 p-3">
+        <div className="size-20 bg-bg-200 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-bg-300 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-tone-100 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-tone-200 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-tone-300 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-accent-1 ring-2 ring-text-100"></div>
+        <div className="size-20 bg-accent-2 ring-2 ring-text-100"></div>
+      </div>
     </main>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
 import ThemeBtn from './header/themeSwitch'
-
 export default function Header() {
   return (
-    <header className='w-full p-4'><ThemeBtn/></header>
+    <header className='flex justify-between w-full px-8 py-4 h-fit'>
+      <nav className='flex gap-4'>
+        <h1 className='text-lg font-bold'>E.E.S.T N°5</h1>
+        <ul className='flex flex-col items-center justify-center bg-bg-200'>
+          <h2 className='text-lg font-bold'>Nosotros</h2>
+          <li>Institucion</li>
+          <li>Autoridades</li>
+          <li>Profesores</li>
+          <li>Pasantias</li>
+          <li>Cooperadora</li>
+        </ul>
+        <ul className='flex flex-col items-center bg-bg-200'>
+          <h2 className='text-lg font-bold'>Especialidades</h2>
+          <li>Electronica</li>
+          <li>Electromecanica</li>
+          <li>Automotor</li>
+        </ul>
+        <ul className='flex flex-col items-center bg-bg-200'>
+          <h2 className='text-lg font-bold'>Otros</h2>
+          <li>Actividades</li>
+          <li>Proyectos</li>
+          <li>Testimonios</li>
+        </ul>
+        <ul className='flex flex-col items-center bg-bg-200'>
+          <h2 className='text-lg font-bold'>Redes Sociales</h2>
+          <li>IG</li>
+          <li>FB</li>
+          <li>TW</li>
+          <li>In</li>
+          <li>Wp</li>
+        </ul>
+        <h2 className='text-lg font-bold'>Novedades</h2>
+      </nav>
+      <div><ThemeBtn/></div></header>
   )
 }
