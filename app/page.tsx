6cec01b7e56be9441f86afb0bb6088d021852d31
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-05-28 11:25:34 -0300

    role based middleware & route.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"; //borrar
+import { useSession } from "next-auth/react";
 import React from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const user = session?.user;
   return (
     <main className="flex flex-col items-center justify-center gap-2 size-full">
-      <h1 className="text-4xl font-extrabold uppercase">	hola soy la home</h1>
+      <h1 className="text-4xl font-extrabold uppercase"> hola soy la home</h1>
+      <h2>user: {user?.name}</h2>
+      <img src={user?.image} alt={user?.name} className="bg-slate-900 p-1 rounded-full" />
+      <h2>mail: {user?.email}</h2>
+      <h2>role: {user?.role}</h2>
       <div className="bg-text-200 size-fit flex gap-4 p-3">
         <div className="size-20 bg-bg-200 ring-2 ring-text-100"></div>
         <div className="size-20 bg-bg-300 ring-2 ring-text-100"></div>
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,22 @@
-export { default } from "next-auth/middleware";
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    /*comprobamos el rol del usuario mediante el token. Si el usuario esta en la url /dashboard , 
+    pero su rol no es 'admin', lo enviamos a la url /denegado*/
+    if (
+      request.nextUrl.pathname.startsWith("/dashboard") &&
+      request.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denegado", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = { matcher: ["/dashboard"] };
