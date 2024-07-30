5f1f9e6e15e1f00e3948d9f7d7495eafb7f13db5
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-07-30 19:38:32 -0300

    replace profesor[ID] with profesorExtra.tsx

--- components/secciones/conocenos/profesores/profesorCard.tsx
+++ components/secciones/conocenos/profesores/profesorCard.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import { FaCirclePlus } from "react-icons/fa6";
 import { Profesor } from "@/types/profesor";
-
-
+import ProfesorExtra from "./profesorExtra";
 
 //interfaz que recibe los datos del objeto profesor, proveniente del array pasado en profesoresList.tsx
 
@@ -13,7 +10,6 @@ export default function ProfesorCard({
 }: {
   profesores: Profesor[];
 }) {
-  
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 xl:px-32 gap-y-2 sm:gap-y-6 justify-items-center">
       {profesores.map((profesor) => (
@@ -24,7 +20,7 @@ export default function ProfesorCard({
           <>
             <Image
               className="rounded-md"
-              src='/imgs/teacherDefault.jpg'
+              src="/imgs/teacherDefault.jpg"
               alt={profesor.name}
               width={150}
               height={150}
@@ -36,13 +32,7 @@ export default function ProfesorCard({
               {profesor.career}
             </h4>
           </>
-          <Link
-            href={`/profesores/${profesor.name}`}
-            className="flex items-center justify-center gap-2 px-2 mt-6 rounded-md bg-bg-300 hover:bg-accent-2"
-          >
-            saber más
-            <FaCirclePlus />
-          </Link>
+          <ProfesorExtra name={profesor.name} description={profesor.description} contact={profesor.contact}/>
         </div>
       ))}
     </section>
---
+++ components/secciones/conocenos/profesores/profesorExtra.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Button } from "@/components/ui/button";
+import { IoArrowRedo } from "react-icons/io5";
+import { MdMail } from "react-icons/md";
+import { FaCirclePlus } from "react-icons/fa6";
+
+export default function ProfesorExtra({
+  name,
+  description,
+  contact,
+}: {
+  name: string;
+  description: string | null;
+  contact: string | null;
+}) {
+  return (
+    <Drawer>
+      <DrawerTrigger className="flex items-center justify-center gap-2 px-2 mt-6 rounded-md bg-bg-300 hover:bg-accent-2">
+        saber más
+        <FaCirclePlus />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Acerca de: {name}</DrawerTitle>
+          <DrawerDescription>{description}</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <AlertDialog>
+            <AlertDialogTrigger className="flex items-center w-full gap-1">
+              <h4 className="flex items-center gap-1 font-bold group text-tone-300 hover:text-accent-2">
+                <MdMail size={24} />
+                enviar correo
+                <i className="transition-transform duration-150 ease-in group-hover:translate-x-2">
+                  <IoArrowRedo />
+                </i>
+              </h4>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Estas segur@?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Esto te redirigirá a tu bandeja de correo para contactarte con
+                  este profesor
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel className="bg-bg-100 hover:bg-accent-2">
+                  Cancelar
+                </AlertDialogCancel>
+                <AlertDialogAction>
+                  <a
+                    className="flex items-center w-full gap-1"
+                    href={`mailto:${contact}`}
+                  >
+                    <h4 className="flex items-center gap-1 font-bold group text-tone-300 hover:text-accent-2">
+                      <MdMail size={24} />
+                      Continuar
+                      <i className="transition-transform duration-150 ease-in group-hover:translate-x-2">
+                        <IoArrowRedo />
+                      </i>
+                    </h4>
+                  </a>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+
+          <DrawerClose>
+            <Button className="bg-bg-100 hover:bg-accent-2" variant="outline">
+              Cerrar
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
