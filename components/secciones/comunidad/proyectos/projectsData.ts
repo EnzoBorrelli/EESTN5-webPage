248832fe95432835b7cdcaa6162e4b5ddc7d8b93
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-09-27 15:30:17 -0300

    Create projectsData.ts

---
+++ components/secciones/comunidad/proyectos/projectsData.ts
@@ -0,0 +1,67 @@
+import autocarrera from './imagenes/autocarrera.jpg'
+import bici from './imagenes/bici.jpeg'
+import radio from './imagenes/radio.jpeg'
+import robot from './imagenes/robot.jpg'
+import desafioEco from './imagenes/desafioEco.jpg'
+import sumo from './imagenes/sumo.jpeg'
+import rover from './imagenes/rover.jpeg'
+import placeholder from './imagenes/placeholder.png'
+
+export const projectsData = [
+    {
+        title: "Radio de industrial @lacincofm",
+        description:
+          "Estacion de radio propia de la industrial de temperley",
+        img: radio,
+        href: "",
+      },
+    {
+        title: "Auto de carreras",
+        description:
+          "Propulsado por paneles solares! fabricado por automotor",
+        img: autocarrera,
+        href: "",
+      },
+    {
+        title: "Robot Led",
+        description:
+          "Creado por el grupo de robotica, robot sumo con luces led",
+        img: robot,
+        href: "",
+      },
+    {
+        title: "Bicileta eléctrica",
+        description:
+          "Bicicleta electrica fabricada por 7mo año",
+        img: bici,
+        href: "",
+      },
+    {
+        title: "Desafio ECO",
+        description:
+          "Desafio llevado a cabo por alumnos de 6to y 7mo año",
+        img: desafioEco,
+        href: "",
+      },
+    {
+        title: "Robot Sumo",
+        description:
+          "Robot autonomo SUMO, construido por el grupo de robotica",
+        img: sumo,
+        href: "",
+      },
+    {
+        title: "Rover R.O.V.E.R.T.A.",
+        description:
+          "Dirigido por Martin Leguizamon, robot autonomo con gps",
+        img: rover,
+        href: "",
+      },
+    {
+        title: "placeholder",
+        description:
+          "Este es un placeholder para agregar mas",
+        img: placeholder,
+        href: "",
+      },
+]
