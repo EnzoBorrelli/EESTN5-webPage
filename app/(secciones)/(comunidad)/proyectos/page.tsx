5df5069a09d54649201fe4a1c2c9690a1ff00e0f
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-09-27 17:44:33 -0300

    fix responsive side

--- app/(secciones)/(comunidad)/proyectos/page.tsx
+++ app/(secciones)/(comunidad)/proyectos/page.tsx
@@ -6,7 +6,7 @@ export default function Proyectos() {
   return (
     <main className="py-10 flex flex-col items-center gap-6 px-2 md:p-10">
       <h1 className="text-4xl mb-12 font-bold text-center">Proyectos</h1>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {projectsData.map((data, index) => (
           <Banner key={index} data={data} moreBtn={false} />
         ))}
