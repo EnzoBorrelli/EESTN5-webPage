83b84e68f8c76f74599951b87979a06aaf42e2d1
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-09-27 17:43:09 -0300

    Create courseData.ts

---
+++ components/secciones/comunidad/cursos/courseData.ts
@@ -0,0 +1,67 @@
+import auto from './imagenes/auto.jpeg'
+import cad from './imagenes/cad.jpeg'
+import carpinteria from './imagenes/carpinteria.jpeg'
+import electricidad from './imagenes/electricidad.jpeg'
+import electronica from './imagenes/electronica.jpeg'
+import moto from './imagenes/moto.jpeg'
+import soldado from './imagenes/soldado.jpeg'
+import torneria from './imagenes/torneria.jpeg'
+
+export const courseData = [
+    {
+        title: "Electricidad Automovil",
+        description:
+          "Aprende como funciona un automovil por dentro",
+        img: auto,
+        href: "",
+      },
+    {
+        title: "CAD diseño asistido",
+        description:
+          "Aprende a diseñar en software de CAD®",
+        img: cad,
+        href: "",
+      },
+    {
+        title: "Carpinteria",
+        description:
+          "Utiliza herramientas y realiza encastres",
+        img: carpinteria,
+        href: "",
+      },
+    {
+        title: "Electricidad",
+        description:
+          "Codigo de colores, voltaje y amperaje",
+        img: electricidad,
+        href: "",
+      },
+    {
+        title: "Inyección Electrónica",
+        description:
+          "Aprende las bases de la Electrónica y el armado de PCBs",
+        img: electronica,
+        href: "",
+      },
+    {
+        title: "Mecánica de motos",
+        description:
+          "aprende a entender las piezas que componen una motocicleta",
+        img: moto,
+        href: "",
+      },
+    {
+        title: "Soldadura",
+        description:
+          "Aprende como soldar de manera segura",
+        img: soldado,
+        href: "",
+      },
+    {
+        title: "Torneria y Fresado",
+        description:
+          "Aprende a utilizar maquinas como el Torno o la fresa",
+        img: torneria,
+        href: "",
+      },
+    ]
