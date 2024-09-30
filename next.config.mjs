30ffbbca63b31ec51c78292be4c5521cfc2a07f9
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-09-30 15:08:38 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xywelhsgabxjaiexuyre.supabase.co', // your Supabase project URL
+        port: '', // leave it empty if no specific port is required
+        pathname: '/storage/v1/object/public/teacher_images/**', // specific path to your images
+      },
+    ],
+  },
+};
 
 export default nextConfig;
