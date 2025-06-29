7d64e1d6a2e189c3ceee7115a8a59771695c0fef
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-29 05:23:27 -0300

    Update page.tsx

--- app/(usuario)/recordatorios/page.tsx
+++ app/(usuario)/recordatorios/page.tsx
@@ -1,25 +1,17 @@
-import { options } from "@/app/api/auth/[...nextauth]/options";
 import EventBadge from "@/components/calendar/eventBadge";
 import NotificationHeader from "@/components/calendar/notificationHeader";
-import { db } from "@/lib/db";
-import { getServerSession } from "next-auth";
+import { exampleEvents } from "@/data/events";
 import React from "react";
 
 export default async function Recordatorios() {
-  const session = await getServerSession(options);
-  const events = await db.reminder.findMany({
-    where: { userId: session?.user?.email },
-    include: { event: true },
-  });
   return (
     <main className="flex flex-col items-center w-full gap-4 p-5 md:p-10 md:px-20">
-      <NotificationHeader userID={session?.user?.email}/>
+      <NotificationHeader />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-4">
-      {events.map((event) => (
+      {exampleEvents.splice(0,3).map((event) => (
         <EventBadge
           key={event.id}
-          event={event.event}
-          reminders={events.map((reminder) => reminder.event)}
+          event={event}
           showDate={true}
         /> 
       ))}
